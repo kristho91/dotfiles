d2025561c12a1cb7047e5d81bfdfb50cfe313244
kristho91 <mail@kristhomsen.dk>
2025-07-21 23:11:02 +0200

    Fixed fish greeting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,12 +3,9 @@ if status is-interactive
 end
 
 function fish_greeting
-    fortune bible | cowsay -f tux
+    fortune cookie | cowsay -f tux
 end
-
-#set -g -x fish_greeting ''
 
 theme_gruvbox dark medium
 
 abbr -a -- cls clear
-abbr -a -- up! sudo dnf upgrade --refresh
